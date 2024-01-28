46740745a2c207f6f98910ca5f471840a9c890e7
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-01-28 12:49:30 +0630

    Added testing in psql connection

--- internal/services/pg_conn_test.go
+++ internal/services/pg_conn_test.go
@@ -1,1 +1,22 @@
 package services_test
+
+import (
+	"testing"
+
+	"github.com/lwinmgmg/user-go/env"
+	"github.com/lwinmgmg/user-go/internal/services"
+)
+
+func TestPgConnection(t *testing.T) {
+	settings, err := env.LoadSettings()
+	if err != nil {
+		t.Errorf("Error on getting settings : %v", err)
+	}
+	conn, err := services.GetPsql(settings.Db)
+	if err != nil {
+		t.Errorf("Error on psql connection : %v", err)
+	}
+	if err := conn.Exec("SELECT 1;").Error; err != nil {
+		t.Errorf("Error on psql query : %v", err)
+	}
+}
