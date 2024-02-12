a65be88118d4be7ae6cefc2507464deb23af0910
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-02-12 23:55:21 +0630

    Adding change password

--- internal/controller/change_password.go
+++ internal/controller/change_password.go
@@ -1,5 +1,18 @@
 package controller
 
-func (ctrl *Controller) ChangePassword(){
+import "github.com/lwinmgmg/user-go/internal/models"
 
+type PasswordChange struct {
+	OldPassword string `form:"old_pass"`
+	NewPassword string `form:"new_pass"`
+}
+
+func (ctrl *Controller) ChangePassword(userCode string, data *PasswordChange) (logintkn LoginToken, err error) {
+	user := models.User{}
+	_, err = user.GetPartnerByCode(userCode, ctrl.RoDb)
+	if err != nil {
+		return
+	}
+	
+	return
 }
